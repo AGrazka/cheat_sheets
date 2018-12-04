9fdc24ec09a8306016b0b66a012a7fcec570c92e
Mateusz Gotkowski <mateusz.gotkowski.91@gmail.com>
2018-12-05 00:05:16 +0100

    modules

---
+++ modules.py
@@ -0,0 +1,11 @@
+import sys
+import builtins
+
+# sys.ps1 == ">>>" w shellu
+# sys.ps2 == "..."
+
+# sys.path      # lista z miejscem gdzie interpreter szuka modułów
+
+dir()           # zwraca listę ze zdefiniowanymi nazwami
+dir(builtins)   # zwraca nazwy wbudowanych funkcji i zmiennych
+
