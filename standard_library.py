b2fa90471a38ca8e704b21671a4ac3e679f1cf1c
Mateusz Gotkowski <mateusz.gotkowski.91@gmail.com>
2018-12-30 13:35:00 +0100

    libraries 1st part

---
+++ standard_library.py
@@ -0,0 +1,32 @@
+import os  # do interakcji z systemem
+
+import glob
+
+glob.glob('*.py')  # lista plików
+
+
+import sys  # do błedów i kończenia programów
+
+# sys.exit()  # kończy program
+sys.stderr.write("blabla")  # wypisuje ostrzeżenie
+
+import re  # do regexów
+
+import math  # do funkcji matematycznych
+
+import random  # do randomów
+
+import statistics  # funkcje statystyczne
+
+import urllib.request  # do pozyskiwania danych z internetu
+
+import smtplib  # do wysyłania maili
+
+import datetime  # data i czas
+
+import zlib  # do kompresji plików
+
+import timeit  # do mierzenia czasu wykonywanych funkcji
+
+import unittest  # do testowania
+
