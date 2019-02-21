85c2ea229481b640469ad11ca44ca6f9430af46f
Mateusz Gotkowski <mateusz.gotkowski.91@gmail.com>
2019-02-21 21:35:14 +0100

    my_split

--- iterators_and _generators.py
+++ iterators_and _generators.py
@@ -10,3 +10,25 @@ def reverse(data):  # tworzy generator
 
 for char in reverse('golf'):
     print(char)
+
+
+test = "test abc cba brrrum aaa aaa czz asc"
+
+
+def my_split(tekst):
+    result = []
+    blank = tekst.index(" ")
+    while blank != -1:
+        result.append(tekst[:blank])
+        tekst = tekst[blank+1:]
+        try:
+            blank = tekst.index(" ")
+        except ValueError:
+            break
+
+    result.append(tekst)
+
+    return result
+
+
+print(my_split(test))
